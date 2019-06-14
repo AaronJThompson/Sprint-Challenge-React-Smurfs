4f1874f83a5900625a16eb439a1456ecd84d9f66
AaronJThompson <aaronjthompson98@gmail.com>
2019-06-14 08:43:44 +0100

    Start NavBar component

---
+++ village/src/components/NavBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+export default function NavBar(props) {
+    let { homePath, formPath } = props;
+    
+}
